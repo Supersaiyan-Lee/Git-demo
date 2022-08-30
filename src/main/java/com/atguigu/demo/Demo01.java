57927cf2eb44f42b08446f5ca719730151fc3bba
Lee <3136666313@qq.com>
2022-08-30 16:23:18 +0800

    新建第一次提交

---
+++ src/main/java/com/atguigu/demo/Demo01.java
@@ -0,0 +1,8 @@
+package com.atguigu.demo;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("新建文件!");
+
+    }
+}
---
+++ src/main/java/com/atguigu/demo/Demo02.java
@@ -0,0 +1,7 @@
+package com.atguigu.demo;
+
+public class Demo02 {
+    public static void main(String[] args) {
+        System.out.println("Demo02");
+    }
+}
